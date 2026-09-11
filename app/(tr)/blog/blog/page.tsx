ae4110f95cbe9bb48f7df1c8310f1c375abd6a77
Woodstonestudio <galibehanim@gmail.com>
2026-09-11 12:14:46 +0200

    Fazladan ic ice blog/blog klasorlerini kaldir

--- app/(tr)/blog/blog/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from "next";
-import { BlogList, blogListMetadata } from "@/components/blog";
-
-export const metadata: Metadata = blogListMetadata("tr");
-
-export default function Page() {
-  return <BlogList locale="tr" />;
-}
